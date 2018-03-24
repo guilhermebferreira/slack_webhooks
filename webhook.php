3472ba058f6b09a799b11fa435b143965351a3f0
Guilherme Ferreira <360guilherme@gmail.com>
2018-03-24 14:26:59 -0300

    improving code identation

--- webhook.php
+++ webhook.php
@@ -1,29 +1,20 @@
 <?php
 
  	$config = include 'config.env';
-  
- 
 
-
-     $mensagem = array(
-                "text"          =>  "@guilherme mensagem enviada usando PHP :slack:"
-            );
+    $mensagem = array(
+        "text"          =>  "@guilherme mensagem enviada usando PHP :slack:"
+    );
 
     $data = json_encode($mensagem);
- 
-
 
     $ch = curl_init($config['webhook']);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-        curl_setopt($ch, CURLOPT_POSTFIELDS, array('payload' => $data));
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        $result = curl_exec($ch);
-        curl_close($ch);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch, CURLOPT_POSTFIELDS, array('payload' => $data));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $result = curl_exec($ch);
+    curl_close($ch);
 
     print_r($result);
-
-
-
-
 
 ?>
