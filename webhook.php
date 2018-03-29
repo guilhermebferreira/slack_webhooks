a40813a0baaa250898123d7d13fb231f996f607f
Guilherme Ferreira <360guilherme@gmail.com>
2018-03-29 12:46:06 -0300

    sending messages with attachments

--- webhook.php
+++ webhook.php
@@ -12,6 +12,43 @@
 
     send_message($data, $config['webhook']);
 
+
+    //enviando uma mensgaem com botões em anexo
+
+    $data = array(
+        "text" => "Os seguintes relatórios estão disponiveis"
+    );
+
+    $actions =
+    [
+        [
+            'type' => "button",
+            'text' => "Report 1",
+            'url' => "https://url.report1"
+        ],
+        [
+            'type' => "button",
+            'text' => "Report 2",
+            'url' => "https://url.report2"
+        ]
+    ];
+
+    $data += [
+        "attachments" =>
+            [
+                [
+                    "fallback" => "More details...", //I only get the message and this text in the slack
+                    'actions' => $actions // or array($actions) 
+
+                ]
+            ]
+    ];
+
+    $payload = json_encode($data);
+
+
+    send_message($payload, $config['webhook']);
+
     
 
 
